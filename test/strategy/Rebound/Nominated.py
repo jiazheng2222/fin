57d90edb430c4b3080608256ad06bfaf071920ef
jiazheng2222 <jiazheng2222@gmail.com>
2017-04-07 17:06:44 +0800

    update

--- test/strategy/Rebound/Nominated.py
+++ test/strategy/Rebound/Nominated.py
@@ -8,25 +8,42 @@ import MySQLdb
 
 from DBConfig import *
 
-def geneCandidate(dbConn):
+def geneNominated(dbConn):
+
+    # candidate list
+    candiList = []
+    lstChgRtoList = []
+    lstDateList = []
 
     dbCur = dbConn.cursor()
     dbCur.execute('SET NAMES utf8;')
     dbCur.execute('SET CHARACTER SET utf8;')
     dbCur.execute('SET character_set_connection=utf8;')
 
-    # Update all the data latest flag to 'N'
-    dbCur.execute("update fin.candidate set latestflag = 'N'")
+    dbCur.execute("select * from  fin.candidate where latestflag = 'Y'")
+    for candiItem in dbCur.fetchall():
+        candiList.append(candiItem[1])
+        lstChgRtoList.append(candiItem[4])
+        lstDateList.append(candiItem[8])
 
     # Load data from tushare
     todayData = ts.get_today_all()
+
     # Initialise variable
     dateToday = date.today().strftime('%Y-%m-%d')
 
     for id, dataItem in todayData.iterrows():
         contractCode, contractName, changePercent, currentPrice, openPrice = dataItem[:5]
+
+        # only check the candidates
+        if contractCode not in candiList:
+            continue
+        loc = candiList.index(contractCode)
+
         lastPrice = dataItem[7]
         volume = dataItem[8]
+
+        # this one may be suspended
         if volume == 0:
             continue
 
@@ -38,34 +55,34 @@ def geneCandidate(dbConn):
             print "Error Data or Market is still running! {0}".format(contractCode)
             break
 
+
         # Check daily change
-        if changePercent < -4.0:
-            # Write to DB
+        # the second day
+        if changePercent < -3.0:
+            # Write to DB : nominated
             # Sample:
-            #('600036', u'招商银行', '3.21', '5.68', '19.01', '18.22', '2017-04-06', '1491478722')
+            #
             timeStamp = datetime.now().strftime("%s")
 
-            print contractCode, contractName, currentPrice-lastPrice,dailyChange,lastPrice
+            print contractCode, contractName, lstChgRtoList[loc],lstDateList[loc], dailyChange, dateToday, timeStamp
 
-            dbCur.execute("INSERT INTO `fin`.`candidate` \
-            (`contractid`, `contractname`, `dailychangenum`, \
-            `dailychangeratio`, `lastprice`, `closeprice`, \
-            `latestflag`, `checkdate`, `createtime`) VALUES \
-            (%s, %s, %s, %s, %s, %s, %s, %s, %s);",
-            (contractCode, contractName, currentPrice-lastPrice,
-            dailyChange,lastPrice, currentPrice, 'Y', dateToday, timeStamp))
+            dbCur.execute("INSERT INTO `fin`.`nominate` \
+            (`contractid`, `contractname`, `lastchangeratio`, \
+            `lastdate`, `currchangeratio`, `checkdate`, `createtime`) VALUES \
+            (%s, %s, %s, %s, %s, %s, %s);",
+            (contractCode, contractName, lstChgRtoList[loc],
+            lstDateList[loc], dailyChange, dateToday, timeStamp))
             dbConn.commit()
 
-def candidate():
+def nominated():
     # create DB connection
     dbConn = MySQLdb.connect(user=DB_USER,
                             passwd=DB_PASSWD,
                             host=DB_HOST,
                             db=DB_DB)
     dbConn.set_character_set('utf8')
-    geneCandidate(dbConn)
+    geneNominated(dbConn)
 
     # close DB
     dbConn.close()
-
 
--- test/strategy/Rebound/candiTrigger.py
+++ test/strategy/Rebound/candiTrigger.py
@@ -7,7 +7,8 @@ from Candidate import candidate
 firstFlag = True
 
 # Warning: run this one before the target time
-
+# 1. skip the first call
+# 2. wait until the target time, and next call will wait for a day
 def triggerCandidate():
     # avoid the short timeslot to call itself
     time.sleep(2)
@@ -15,15 +16,16 @@ def triggerCandidate():
     global firstFlag
     currTime = datetime.datetime.today()
 
-    # first day
+    #if currTime.hour < 17 and currTime.minute < 30:
     if currTime.hour < 16:
+        # avoid to call it next time
         runTime = currTime.replace(hour = 16, minute = 0, second=0 , microsecond=0)
     else:
         runTime = currTime + datetime.timedelta(days=1)
     delta_t = runTime - currTime
-    secs=delta_t.seconds
+    secs=delta_t.total_seconds()
 
-    print "Wait for {0} seconds".format(secs), runTime
+    print "Wait for {0} seconds".format(secs), runTime,
     Timer(secs, triggerCandidate).start()
     # Ignore the first call
     if firstFlag == True:
